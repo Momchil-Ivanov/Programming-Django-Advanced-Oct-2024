ea3f48cc8887ae5ddd00bea74929f79a2bd3b133
Momchil Ivanov <momchil2207@abv.bg>
2024-11-24 18:16:03 +0200

    feature: added emails

--- excelTipsAndTricks/accounts/urls.py
+++ excelTipsAndTricks/accounts/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView
+from django.views.generic import TemplateView
+
 from . import views
-#
+
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path('password-change/', views.CustomPasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', TemplateView.as_view(template_name='accounts/password-change-done.html'), name='password_change_done'),
+    path('profile/', views.ProfilePageView.as_view(), name='profile'),  # Profile page
+    path('profile/update/', views.ProfileUpdateView.as_view(), name='profile_update'),  # Profile update page
+    path('profile/delete/', views.ProfileDeleteView.as_view(), name='profile_delete'),  # Profile delete page
 ]
--- excelTipsAndTricks/accounts/views.py
+++ excelTipsAndTricks/accounts/views.py
@@ -1,24 +1,98 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.contrib import messages
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, UpdateView
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import Group
+from .forms import CustomUserCreationForm, ProfileUpdateForm, CustomPasswordChangeForm
+from django.contrib.auth import authenticate
 
 
+# Registration View
 class RegisterView(FormView):
     template_name = 'accounts/register-page.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')  # Redirect to login page after successful registration
+    form_class = CustomUserCreationForm  # Use custom form
+    success_url = reverse_lazy('login')
 
     def form_valid(self, form):
-        form.save()  # Save the user to the database
+        form.save()
+        messages.success(self.request, "Registration successful. You can now log in.")
         return super().form_valid(form)
 
 
+# Custom Login View
 class CustomLoginView(LoginView):
     template_name = 'accounts/login-page.html'
 
     def form_invalid(self, form):
-        # Adding custom message to the message framework
         messages.error(self.request, "Account or Password are not correct")
         return super().form_invalid(form)
+
+    def form_valid(self, form):
+        remember_me = self.request.POST.get('remember_me')  # Get checkbox value
+        if not remember_me:
+            # Session will expire when the browser is closed
+            self.request.session.set_expiry(0)
+        else:
+            # Session will expire in 30 days
+            self.request.session.set_expiry(2592000)
+        return super().form_valid(form)
+
+
+# Profile Page View
+class ProfilePageView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile-page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Get the user's profile information
+        user_profile = self.request.user.profile
+        context['user_profile'] = user_profile
+
+        # Get the user's group/roles
+        user_groups = self.request.user.groups.all()
+        context['user_groups'] = user_groups
+
+        return context
+
+
+# Profile Update View
+class ProfileUpdateView(LoginRequiredMixin, UpdateView):
+    template_name = 'accounts/profile-update.html'
+    form_class = ProfileUpdateForm
+    success_url = reverse_lazy('profile')
+
+    def get_object(self):
+        # Fetching the profile of the logged-in user
+        return self.request.user.profile
+
+    def get_form_kwargs(self):
+        # Pass the current user's email to the form as initial data
+        kwargs = super().get_form_kwargs()
+        kwargs['initial'] = {'email': self.request.user.email}
+        return kwargs
+
+
+# Password Change View
+class CustomPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'accounts/password-change.html'
+    form_class = CustomPasswordChangeForm
+    success_url = reverse_lazy('password_change_done')
+
+
+class ProfileDeleteView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile-delete.html'
+
+    def post(self, request, *args, **kwargs):
+        password = request.POST.get('password')
+        user = authenticate(username=request.user.username, password=password)
+        if user is not None:
+            # If authentication is successful, delete the user and their profile
+            request.user.delete()
+            messages.success(request, 'Your profile has been deleted successfully.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Incorrect password. Please try again.')
+            return redirect('profile-delete')  # Redirect back to the delete profile page
